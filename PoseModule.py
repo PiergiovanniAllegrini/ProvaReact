917e6fea3228d24b41c9cf5987496228b239a0d4
Piergiovanni?git config --global user.name Piergiovannigit config --global user.name Piergiovanni? <piergiovanniallegrini@gmail.com>
2022-02-12 23:58:11 +0100

    Restaurato PoseModule

--- PoseModule.py
+++ PoseModule.py
@@ -3,7 +3,7 @@ import cv2
 import mediapipe as mp
 import time
 
-"""class poseDetector():
+class poseDetector():
     def __init__(self, mode=False, upBody=False, smooth=True,
                  detectionCon=0.5, trackCon=0.5):
         self.mode = mode
@@ -14,7 +14,7 @@ import time
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
-                                     self.detectionCon, self.trackCon) """
+                                     self.detectionCon, self.trackCon) 
     def findPose(self, img, draw=True): 
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRGB)
